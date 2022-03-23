9fdf0a13cd9ccd20203a7c9976a919e73e1067d7
Harry Tunstall <43201967+htunstall@users.noreply.github.com>
2022-03-23 12:15:25 +0000

    Create lookup.py
    Moved lookup values to different file, so they are located in only one place

---
+++ lookup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 12:30:35 2022
+
+@author: Harry
+"""
+
+month_str_to_number = {"jan" : 1, "feb" : 2, "mar" : 3, "apr" : 4, "may" : 5, "jun" : 6, "jul" : 7, "aug" : 8, "sep" : 9, "oct" : 10, "nov" : 11, "dec" : 12}
+months_list         = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+valid_categories    = ["GROC", "HOME", "TAKE", "PERS", "MISC", "EAT", "FUEL", "SUB", "REB", "BILL", "VET", "MORT", "PHON", "PETS", "CAR"]
